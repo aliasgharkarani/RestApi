59cd750ebea1576f2630818dee3a65e1068fd05f
aliasgharkarani <aliasgharkarani125@hotmail.com>
2018-11-27 14:18:36 +0500

    Update and Delete Done

--- routes/api.js
+++ routes/api.js
@@ -2,34 +2,38 @@ const express = require('express');
 const router = express.Router();
 const Model = require('./../Schema')
 //Geting Data
-router.get('/ninjas', function (req, res,next) {
+router.get('/ninjas', function (req, res, next) {
     res.send(JSON.stringify({ type: "GET" }))
 })
 
 //Adding Data
-router.post('/ninjas', function (req, res,next) {
+router.post('/ninjas', function (req, res, next) {
     // console.log(req.body);
     // var ninja=new Model(req.body);
     // ninja.save();
-    
+
     //Its Returns A Promise
-    Model.create(req.body).then(function(ninja){
+    Model.create(req.body).then(function (ninja) {
         res.send(ninja)
     }).catch(next)
 
 })
 
 //Updating Data
-router.put('/ninjas/:id', function (req, res,next) {
-    res.send({ type: "PUT" })
+router.put('/ninjas/:id', function (req, res, next) {
+    Model.findByIdAndUpdate({ _id: req.params.id },req.body).then(function () {
+      Model.findOne({ _id: req.params.id }).then(function(ninja){
+          res.send(ninja)
+      })
+    });
 })
 
 //Delete Data
-router.delete('/ninjas/:id', function (req, res,next) {
-// console.log(req.params.id)
-Model.findByIdAndRemove({_id:req.params.id}).then(function(ninja){
-res.send(ninja)
-});
+router.delete('/ninjas/:id', function (req, res, next) {
+    // console.log(req.params.id)
+    Model.findByIdAndRemove({ _id: req.params.id }).then(function (ninja) {
+        res.send(ninja)
+    });
 });
 
 module.exports = router;
